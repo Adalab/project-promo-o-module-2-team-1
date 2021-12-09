a4215991cc9ddbae8e7e43061a066ed183016b61
Mara Rocha Fernández <maranhadisenha@gmail.com>
2021-12-09 14:23:12 +0100

    update main.js and docs folder

--- src/js/main.js
+++ src/js/main.js
@@ -3,27 +3,29 @@
 const boxArrowElement = document.querySelector('.js-box__arrow');
 const boxMainElement = document.querySelector('.js-box__main');
 
-
 boxArrowElement.addEventListener('click', handlerArrowClick);
 
 function handlerArrowClick() {
-    if (boxArrowElement.classList.contains('fa-chevron-up')) {
-        boxArrowElement.classList.remove('fa-chevron-up');
-        boxArrowElement.classList.add('fa-chevron-down');
-        showContent();
-    } else {
-        boxArrowElement.classList.remove('fa-chevron-down');
-        boxArrowElement.classList.add('fa-chevron-up');
-        showContent();
-    }
+  if (boxArrowElement.classList.contains('fa-chevron-up')) {
+    boxArrowElement.classList.remove('fa-chevron-up');
+    boxArrowElement.classList.add('fa-chevron-down');
+    showContent();
+  } else {
+    boxArrowElement.classList.remove('fa-chevron-down');
+    boxArrowElement.classList.add('fa-chevron-up');
+    showContent();
+  }
 }
 
 function showContent() {
-    if (boxMainElement.classList.contains('hidden')) {
-        // borrar la clase .hidden para mostrar el contenido
-        boxMainElement.classList.remove('hidden');
-    } else {
-        // añadir la clase .hidden para ocultar el contenido
-        boxMainElement.classList.add('hidden');
-    }
+  boxMainElement.classList.toggle('hidden');
+  /*
+  if (boxMainElement.classList.contains('hidden')) {
+    // borrar la clase .hidden para mostrar el contenido
+    boxMainElement.classList.remove('hidden');
+  } else {
+    // añadir la clase .hidden para ocultar el contenido
+    boxMainElement.classList.add('hidden');
+  }
+  */
 }
