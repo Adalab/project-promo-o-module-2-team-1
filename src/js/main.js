7a89841d0885fbe4f147a77f0b375b438a628718
Mara Rocha Fernández <maranhadisenha@gmail.com>
2021-12-09 17:14:03 +0100

    update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,32 @@
 'use strict';
 
+// obtener elemento html con clase .js-box__arrow (flecha)
 const boxArrowElement = document.querySelector('.js-box__arrow');
 
+// escuchar y manejar evento click
 boxArrowElement.addEventListener('click', handlerArrowClick);
 
 function handlerArrowClick(event) {
-  const arrow = event.currentTarget;
-  if (arrow.classList.contains('fa-chevron-up')) {
-    arrow.classList.remove('fa-chevron-up');
-    arrow.classList.add('fa-chevron-down');
-    showBoxMain();
+  // función cambiar icono flecha
+  changeBoxArrow(event.currentTarget);
+  // función mostrar/ocultar contenido
+  showBoxMain();
+}
+
+function changeBoxArrow(arrowElement) {
+  // si contiene el icono flecha-arriba lo cambia por el icono flecha-abajo y viceversa
+  if (arrowElement.classList.contains('fa-chevron-up')) {
+    arrowElement.classList.remove('fa-chevron-up');
+    arrowElement.classList.add('fa-chevron-down');
   } else {
-    arrow.classList.remove('fa-chevron-down');
-    arrow.classList.add('fa-chevron-up');
-    showBoxMain();
+    arrowElement.classList.remove('fa-chevron-down');
+    arrowElement.classList.add('fa-chevron-up');
   }
 }
 
 function showBoxMain() {
+  // obtener elemento html con clase .js-box__main (contenido)
   const boxMainElement = document.querySelector('.js-box__main');
+  // eliminar/añadir clase .hidden
   boxMainElement.classList.toggle('hidden');
 }
