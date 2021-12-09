192c969352475199e60b5012ec52b5e9a946584d
Mara Rocha Fernández <maranhadisenha@gmail.com>
2021-12-09 20:09:32 +0100

    update form css styles

--- src/js/main.js
+++ src/js/main.js
@@ -7,26 +7,30 @@ const boxArrowElement = document.querySelector('.js-box__arrow');
 boxArrowElement.addEventListener('click', handlerArrowClick);
 
 function handlerArrowClick(event) {
-  // función cambiar icono flecha
+  // función: cambiar icono flecha
   changeBoxArrow(event.currentTarget);
-  // función mostrar/ocultar contenido
+
+  // función: mostrar/ocultar contenido
   showBoxMain();
 }
 
 function changeBoxArrow(arrowElement) {
-  // si contiene el icono flecha-arriba lo cambia por el icono flecha-abajo y viceversa
+  // si contiene el icono flecha-arriba
   if (arrowElement.classList.contains('fa-chevron-up')) {
-    arrowElement.classList.remove('fa-chevron-up');
+    // añadir icono flecha-abajo y borrar icono flecha-arriba
     arrowElement.classList.add('fa-chevron-down');
+    arrowElement.classList.remove('fa-chevron-up');
   } else {
-    arrowElement.classList.remove('fa-chevron-down');
+    // añadir icono flecha-arriba y borrar icono flecha-abajo
     arrowElement.classList.add('fa-chevron-up');
+    arrowElement.classList.remove('fa-chevron-down');
   }
 }
 
 function showBoxMain() {
   // obtener elemento html con clase .js-box__main (contenido)
   const boxMainElement = document.querySelector('.js-box__main');
+
   // eliminar/añadir clase .hidden
   boxMainElement.classList.toggle('hidden');
 }
