b6b9d80aa95cf0781111e1794e3e610d76e2ceaf
Mara Rocha Fernández <maranhadisenha@gmail.com>
2021-12-09 14:08:30 +0100

    add js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-console.log('>> Ready :)');
+const boxArrowElement = document.querySelector('.js-box__arrow');
+const boxMainElement = document.querySelector('.js-box__main');
+
+
+boxArrowElement.addEventListener('click', handlerArrowClick);
+
+function handlerArrowClick() {
+    if (arrowElement.classList.contains('fa-chevron-up')) {
+        arrowElement.classList.remove('fa-chevron-up');
+        arrowElement.classList.add('fa-chevron-down');
+        showContent();
+    } else {
+        arrowElement.classList.remove('fa-chevron-down');
+        arrowElement.classList.add('fa-chevron-up');
+        showContent();
+    }
+}
+
+function showContent() {
+    if (boxMainElement.classList.contains('hidden')) {
+        // borrar la clase .hidden para mostrar el contenido
+        boxMainElement.classList.remove('hidden');
+    } else {
+        // añadir la clase .hidden para ocultar el contenido
+        boxMainElement.classList.add('hidden');
+    }
+}
