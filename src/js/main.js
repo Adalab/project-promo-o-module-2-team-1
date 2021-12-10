583babfb391a8870005e10b01bf78f58e38bf960
Mara Rocha Fernández <maranhadisenha@gmail.com>
2021-12-10 12:30:18 +0100

    update project

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+// DISEÑA
+// obtener elemento html legend de diseña
+// escuchar y manejar el elemento obtenido
+// cambiar icono flecha de diseña
+// ocultar todas las section
+// mostrar la sección diseña
+
+// RELLENA
+// obtener elemento html legend de rellena
+// escuchar y manejar el elemento obtenido
+// cambiar icono flecha de rellena
+// ocultar todas las section
+// mostrar la sección rellena
+
+// COMPARTE
+// obtener elemento html legend de comparte
+// escuchar y manejar el elemento obtenido
+// cambiar icono flecha de comparte
+// ocultar todas las section
+// mostrar la sección comparte
