67f0456c28fdd914c0e7e53ccc763e24c734f314
Mara Rocha Fernández <maranhadisenha@gmail.com>
2021-12-10 16:26:18 +0100

    update css styles

--- src/js/main.js
+++ src/js/main.js
@@ -20,36 +20,36 @@ const elementIcon3 = document.querySelector('.js-iconShare');
 // Funciones
 
 function handlerClickLegend() {
-    elementSection1.classList.toggle('hidden');
-    if (elementSection1.classList.contains('hidden')) {
-        elementIcon1.classList.remove('fa-chevron-up');
-        elementIcon1.classList.add('fa-chevron-down');
-    } else {
-        elementIcon1.classList.remove('fa-chevron-down');
-        elementIcon1.classList.add('fa-chevron-up');
-    }
+  elementSection1.classList.toggle('hidden');
+  if (elementSection1.classList.contains('hidden')) {
+    elementIcon1.classList.remove('fa-chevron-up');
+    elementIcon1.classList.add('fa-chevron-down');
+  } else {
+    elementIcon1.classList.remove('fa-chevron-down');
+    elementIcon1.classList.add('fa-chevron-up');
+  }
 }
 
 function handlerClickLegend2() {
-    elementSection2.classList.toggle('hidden');
-    if (elementSection2.classList.contains('hidden')) {
-        elementIcon2.classList.remove('fa-chevron-up');
-        elementIcon2.classList.add('fa-chevron-down');
-    } else {
-        elementIcon2.classList.remove('fa-chevron-down');
-        elementIcon2.classList.add('fa-chevron-up');
-    }
+  elementSection2.classList.toggle('hidden');
+  if (elementSection2.classList.contains('hidden')) {
+    elementIcon2.classList.remove('fa-chevron-up');
+    elementIcon2.classList.add('fa-chevron-down');
+  } else {
+    elementIcon2.classList.remove('fa-chevron-down');
+    elementIcon2.classList.add('fa-chevron-up');
+  }
 }
 
 function handlerClickLegend3() {
-    elementSection3.classList.toggle('hidden');
-    if (elementSection3.classList.contains('hidden')) {
-        elementIcon3.classList.remove('fa-chevron-up');
-        elementIcon3.classList.add('fa-chevron-down');
-    } else {
-        elementIcon3.classList.remove('fa-chevron-down');
-        elementIcon3.classList.add('fa-chevron-up');
-    }
+  elementSection3.classList.toggle('hidden');
+  if (elementSection3.classList.contains('hidden')) {
+    elementIcon3.classList.remove('fa-chevron-up');
+    elementIcon3.classList.add('fa-chevron-down');
+  } else {
+    elementIcon3.classList.remove('fa-chevron-down');
+    elementIcon3.classList.add('fa-chevron-up');
+  }
 }
 
 // escuchar y manejar el elemento obtenido
@@ -64,16 +64,16 @@ elementLegend3.addEventListener('click', handlerClickLegend3);
 // mostrar la sección diseña
 
 
-//RELLENA 
+//RELLENA
 
 const inputName = document.querySelector('.js-name');
 
 
-function handlerInputName(){
-    const cardName = document.querySelector('.js-cardName');
-    if ( inputName.value === '' ) {
-        cardName.innerHTML = 'Nombre Apellido';
-    } else {cardName.innerHTML = inputName.value;}
+function handlerInputName() {
+  const cardName = document.querySelector('.js-cardName');
+  if (inputName.value === '') {
+    cardName.innerHTML = 'Nombre Apellido';
+  } else { cardName.innerHTML = inputName.value; }
 }
 
 inputName.addEventListener('keyup', handlerInputName);
