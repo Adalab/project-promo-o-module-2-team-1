7a0f421b68f71984950efd2fc0cb8ea5584af1cd
Sandra Gutierrez <sandsan12@gmail.com>
2021-12-10 12:58:51 +0100

    Add collapsed legend 1

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,31 @@
 
 // DISEÑA
 // obtener elemento html legend de diseña
+
+const elementLegend1 = document.querySelector('.js-legendDesign');
+const elementSection1 = document.querySelector('.js-sectionDesign');
+const elementIcon1 = document.querySelector('.js-iconDesign');
+
+
+// Funciones
+
+function handlerClickLegend(){
+    elementSection1.classList.toggle('hidden');
+    if ( elementSection1.classList.contains('hidden') ){
+        elementIcon1.classList.remove('fa-chevron-up');
+        elementIcon1.classList.add('fa-chevron-down');
+    } else{
+        elementIcon1.classList.remove('fa-chevron-down');
+        elementIcon1.classList.add('fa-chevron-up');
+    }
+}
+
 // escuchar y manejar el elemento obtenido
+
+elementLegend1.addEventListener('click', handlerClickLegend);
+
+
+
 // cambiar icono flecha de diseña
 // ocultar todas las section
 // mostrar la sección diseña
