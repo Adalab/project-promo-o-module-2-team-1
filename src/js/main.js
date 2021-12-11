2d72263f8183eb71a352a325ce20e704402d949a
Mariamafalda2021 <mariarezgarcia@gmail.com>
2021-12-11 12:05:15 +0100

    tel

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+const phoneInput = document.querySelector(".js-fill_phone")
+const phonePreview = document.querySelector(".js-phonePreview");
+
+
+function handlerPreviewPhone() {
+    console.log(phoneInput.value);
+    phonePreview.href = `tel:${phoneInput.value}`;
+}
+
+phoneInput.addEventListener("keyup", handlerPhonePreview);
